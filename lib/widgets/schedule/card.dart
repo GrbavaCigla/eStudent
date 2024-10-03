6fa654bc09a18f0693395fcde1a2e311e9b99109
Aleksa Ognjanovic <alexa.ognjanovic@protonmail.com>
2024-10-03 20:25:44 +0200

    Animation and accordion for cards.

--- lib/widgets/schedule/card.dart
+++ lib/widgets/schedule/card.dart
@@ -3,65 +3,111 @@ import 'package:flutter/material.dart';
 import 'package:estudent/models/subject.dart';
 import 'package:estudent/constants.dart';
 
-class ScheduleCard extends StatelessWidget {
+class ScheduleCard extends StatefulWidget {
   final Subject subject;
 
   const ScheduleCard({super.key, required this.subject});
 
   @override
+  State<ScheduleCard> createState() => _ScheduleCardState();
+}
+
+class _ScheduleCardState extends State<ScheduleCard> {
+  bool _isOpen = false;
+
+  @override
   Widget build(BuildContext context) {
-    return Card(
-      child: Padding(
-        padding: const EdgeInsets.all(kScheduleCardPadding),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text(
-                  subject.time?.trim() ?? "",
-                  style: Theme.of(context)
-                      .textTheme
-                      .bodyLarge
-                      ?.copyWith(color: Theme.of(context).disabledColor),
-                ),
-                Text(
-                  subject.group ?? "",
-                  style: Theme.of(context)
-                      .textTheme
-                      .bodyLarge
-                      ?.copyWith(color: Theme.of(context).disabledColor),
-                ),
-              ],
+    List<Widget>? items =
+        widget.subject.lecturers?.map((elem) => Text(elem)).toList();
+    items = items == null
+        ? []
+        : [
+            SizedBox(
+              height: kScheduleCardSpacing,
             ),
-            SizedBox(height: kScheduleCardSpacing),
-            RichText(
-              text: TextSpan(
-                children: [
-                  TextSpan(
-                    text:
-                        subject.code == null ? "" : "${subject.code?.trim()} ",
-                    style: Theme.of(context).textTheme.headlineSmall?.copyWith(
-                          fontWeight: FontWeight.bold,
-                          color: Theme.of(context).disabledColor,
-                        ),
+            ...items
+          ];
+
+    return Card(
+      child: InkWell(
+        customBorder: Theme.of(context).cardTheme.shape,
+        onTap: () => setState(() {
+          _isOpen = !_isOpen;
+        }),
+        child: Padding(
+          padding: const EdgeInsets.all(kScheduleCardPadding),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        widget.subject.time?.trim() ?? "",
+                        style: Theme.of(context)
+                            .textTheme
+                            .bodyLarge
+                            ?.copyWith(color: Theme.of(context).disabledColor),
+                      ),
+                      Text(
+                        widget.subject.group ?? "",
+                        style: Theme.of(context)
+                            .textTheme
+                            .bodyLarge
+                            ?.copyWith(color: Theme.of(context).disabledColor),
+                      ),
+                    ],
                   ),
-                  TextSpan(
-                    text: subject.name?.trim() ?? "",
-                    style: Theme.of(context).textTheme.headlineSmall?.copyWith(
-                          fontWeight: FontWeight.bold,
+                  SizedBox(height: kScheduleCardSpacing),
+                  RichText(
+                    text: TextSpan(
+                      children: [
+                        TextSpan(
+                          text: widget.subject.code == null
+                              ? ""
+                              : "${widget.subject.code?.trim()} ",
+                          style: Theme.of(context)
+                              .textTheme
+                              .headlineSmall
+                              ?.copyWith(
+                                fontWeight: FontWeight.bold,
+                                color: Theme.of(context).disabledColor,
+                              ),
                         ),
+                        TextSpan(
+                          text: widget.subject.name?.trim() ?? "",
+                          style: Theme.of(context)
+                              .textTheme
+                              .headlineSmall
+                              ?.copyWith(
+                                fontWeight: FontWeight.bold,
+                              ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  SizedBox(height: kScheduleCardSpacing),
+                  Text(
+                    widget.subject.place?.trim() ?? "",
+                    style: Theme.of(context).textTheme.titleMedium,
+                  ),
+                ] +
+                [
+                  AnimatedSwitcher(
+                    duration: Durations.short2,
+                    transitionBuilder: (child, animation) {
+                      return SizeTransition(
+                          sizeFactor: animation, child: child);
+                    },
+                    child: (_isOpen
+                        ? Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: items,
+                          )
+                        : null),
                   ),
                 ],
-              ),
-            ),
-            SizedBox(height: kScheduleCardSpacing),
-            Text(
-              subject.place?.trim() ?? "",
-              style: Theme.of(context).textTheme.titleMedium,
-            ),
-          ],
+          ),
         ),
       ),
     );
