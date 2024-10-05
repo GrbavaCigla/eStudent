00cbf877693a72175442a011ac88b951ca39b122
Aleksa Ognjanovic <alexa.ognjanovic@protonmail.com>
2024-10-05 15:29:14 +0200

    Migrate to newer flutter version

--- android/app/src/main/kotlin/com/example/estudent/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.estudent
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
---
+++ android/app/src/main/kotlin/org/grbavacigla/estudent/estudent/MainActivity.kt
@@ -0,0 +1,5 @@
+package org.grbavacigla.estudent.estudent
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
